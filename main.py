647856090e5c63f282fd529a55b654a65314190d
JSM <smjeong5090@naver.com>
2024-09-12 14:33:14 +0900

    Add btn2(Clear) and handler function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys # 시스템 제어 관련 모듈
 
 # 위젯이란? GUI 프로그램에서 구성요소를 뜻하는 용어
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit) # QMessageBox : 메시지박스 위젯
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout) # QMessageBox : 메시지박스 위젯
 from PyQt5.QtGui import QIcon                                                              # Icon을 추가하기 위한 라이브러리
 
 # 나는 계산기 유형을 직접 정의한다! 이때 Qwidget에 기반을 둔다
@@ -13,17 +13,29 @@ class Calculator(QWidget) :
 
     def initUI(self) :
 
+
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
 
         self.btn1 = QPushButton('Message', self)                                           # 버튼추가
         self.btn1.clicked.connect(self.activateMessage)                                    # 이벤트 핸들링 : 클릭했을 때, 뭐를 할거다! 라고 정하는 것
+        
+        self.btn2 = QPushButton('Clear', self)
+        self.btn2.clicked.connect(self.clearMessage)
 
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+
+       
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)                                                               # 수직 레이아웃 위젯 생성
-        vbox.addStretch(1)                                                                 # 빈 공간
+        vbox.addLayout(hbox)                                                                 # 빈 공간
+        vbox.addStretch(1)
         vbox.addWidget(self.btn1)                                                          # 버튼 위치
-        vbox.addStretch(1)                                                                 # 빈 공간
+        vbox.addStretch(1)
+                                                                         # 빈 공간
 
         self.setLayout(vbox)                                                               # 설정 적용
 
@@ -31,6 +43,9 @@ class Calculator(QWidget) :
         self.setWindowIcon(QIcon('icon.png'))                                              # 윈도 아이콘 추가
         self.resize(256, 256)
         self.show()
+    
+    def clearMessage(self) :
+        self.te1.clear()
 
     def activateMessage(self) :
         self.te1.appendPlainText("Hello, PyQt App!")                                                            # 버튼을 클릭할 때 동작하는 함수 : 메세지 박스 출력
